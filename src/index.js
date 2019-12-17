9db555f656391dfe30b9a763669bfaad092462b6
Leiya Kenney <leiya.kenney@gmail.com>
2019-12-17 10:18:27 -0700

    Successfully GET top word and its count on the right side of the page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.ajax({
+    url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
+    success: function(data){
+      $('.word-count').append(`The most popular word is ${Object.keys(data.word)} with a frequency of ${Object.values(data.word)}`)
+    },
+    error: function(error){
+      console.log(error)
+    }
+  })
 })
