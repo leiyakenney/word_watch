8bf7888b81b090785c12a4710d90d1867e78f61a
Leiya Kenney <leiya.kenney@gmail.com>
2019-12-17 11:35:32 -0700

    Create successful POST, but still have to refresh

--- src/index.js
+++ src/index.js
@@ -4,10 +4,28 @@ $(document).ready(() => {
   $.ajax({
     url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
     success: function(data){
-      $('.word-count').append(`The most popular word is ${Object.keys(data.word)} with a frequency of ${Object.values(data.word)}`)
+      $(".word-count").append(`"${Object.keys(data.word)}" sample: ${Object.values(data.word)}`)
     },
     error: function(error){
       console.log(error)
     }
+  }),
+
+  $("#breakdown").click(function() {
+    let text = $("#submission").val()
+
+    $.ajax({
+      type: "POST",
+      url: "https://wordwatch-api.herokuapp.com/api/v1/words",
+      data: JSON.stringify({ word: { value: `${text}`}}),
+      contentType: "application/json",
+      success: function(response) {
+        alert("sample added")
+        $("#submission").val("")
+      },
+      error: function(error){
+        console.log(error)
+      }
+    })
   })
 })
